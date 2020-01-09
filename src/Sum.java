86590fe97a818ec30072817f2436c9512a38fa47
thechaotic <mixnba@mail.ru>
2020-01-09 19:58:03 +0300

    сумма двух чисел

---
+++ src/Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите первое число: ");
+        int a = in.nextInt();
+        System.out.println("введите второе число: ");
+        int b = in.nextInt();
+        System.out.println(a + b);
+
+    }
+}
